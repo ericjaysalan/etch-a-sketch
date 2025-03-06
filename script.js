603815d24ec0b1ea249f9e4a035cc7bbaa5a2586
Eric Jay <44886343+ericjaysalan@users.noreply.github.com>
2025-03-06 12:52:31 +0800

    Make a canvas with 16x16 divs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-log("Hello World!");
+const canvas = document.querySelector("#container");
+
+function createCanvas(canvasWidthAndHeight) {
+  for (let row = 0; row < canvasWidthAndHeight; row++) {
+    for (let square = 0; square < canvasWidthAndHeight; square++) {
+      let div = document.createElement("div");
+      div.textContent = "?";
+      div.classList.toggle("square");
+      canvas.appendChild(div);
+    }
+  }
+}
+
+createCanvas(16);
