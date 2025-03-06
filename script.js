91b193ec44e1ed23d422d7d0f25f9da72978f885
Eric Jay <44886343+ericjaysalan@users.noreply.github.com>
2025-03-06 13:28:50 +0800

    Implement basic sketching

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const canvas = document.querySelector("#container");
+const COLOR_WHEN_HOVERED = "red";
+
+function colorTheSquare(div, color) {
+  div.style.backgroundColor = color;
+  div.style.color = color;
+}
+
+function etch(event) {
+  const square = event.target;
+
+  colorTheSquare(square, COLOR_WHEN_HOVERED);
+}
 
 function createCanvas(canvasWidthAndHeight) {
   for (let row = 0; row < canvasWidthAndHeight; row++) {
@@ -6,6 +18,8 @@ function createCanvas(canvasWidthAndHeight) {
       let div = document.createElement("div");
       div.textContent = "?";
       div.classList.toggle("square");
+      div.addEventListener("mouseover", etch);
+
       canvas.appendChild(div);
     }
   }
